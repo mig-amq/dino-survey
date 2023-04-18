f9a6dc488988b409eca8784526fc75301850b162
mig-amq <miguel_quiambao@dlsu.edu.ph>
2023-04-18 23:22:16 +0800

    initial test

---
+++ js/components/Card.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    data() {
+      return {
+        quid: "",
+        question: "",
+        answers: "",
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
---
+++ js/script.js
@@ -0,0 +1,6 @@
+import Card from './components/Card.vue'
+$(document).ready(() => {
+  const { createApp } = Vue
+  alert(1)
+
+})
